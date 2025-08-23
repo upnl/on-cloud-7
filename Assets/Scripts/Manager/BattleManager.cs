92b3388f03cf1873d7ec457ee40a8908211db497
Dantae <01030263252a@gmail.com>
2025-08-24 06:06:22 +0900

    chore: BattleManager TODO 주석 추가

--- Assets/Scripts/Manager/BattleManager.cs
+++ Assets/Scripts/Manager/BattleManager.cs
@@ -35,6 +35,17 @@ namespace OnCloud7
         /// 적의 다음 스킬 순서
         /// </summary>
         private int _enemySkillIndex;
+        
+        /*
+         * BattleManager TODO:
+         * 1. BattleManager.Initialize() 호출
+         * 2. 깨달음 선택 후 LoadNextRound() 호출하며 다음 라운드 준비 (UI도 변경)
+         * 3. 현재 남은 체력 등의 정보들을 UI에 표시
+         * 4. Roll()의 결과로 나온 CheckResult()의 gains들을 BattleManager에 넘겨서 수치 변동에 반영할 수 있도록
+         * 5. 라운드 끝날 때 UpgradeLevel()로 업그레이드 레벨 확인 후 RoundUpgradeTemplate에서 불러와서 증강체 3개 선택지 띄우기
+         * 6. 적이 스킬을 돌아가면서 쓰도록 하기
+         * 7. 적 특수 패턴 (예: 군대의 전역 등)
+         */
 
         public void Initialize()
         {
