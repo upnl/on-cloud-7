dd8bd6ecc6517cb981989463efe4738aa9f068aa
choco <rlarldusgood@naver.com>
2025-08-23 21:48:32 +0900

    feat: special symbols applied

--- Assets/Scripts/MachineModel.cs
+++ Assets/Scripts/MachineModel.cs
@@ -25,11 +25,17 @@ public class MachineModel
         _symbolPool = new List<SymbolModel>();
         for (int i = 0; i < 3; i++)
         {
-            for (int j = 0; j < 9; j++)
+            for (int j = 0; j < 6; j++)
             {
                 _symbolPool.Add(new SymbolModel(GameManager.Instance.SymbolTemplates[i]));
             }
+
+            for (int j = 0; j < 3; j++)
+            {
+                _symbolPool.Add(new SymbolModel(GameManager.Instance.SymbolTemplates[i + 3]));
+            }
         }
+        _symbolPool.Add(new SymbolModel(GameManager.Instance.SymbolTemplates[6]));
         _result = new List<SymbolModel>();
     }
     
@@ -51,11 +57,93 @@ public class MachineModel
 
     public void CheckResult(List<SymbolModel> result)
     {
-        int Atkgain = 0;
-        int MPgain = 0;
-        int Avoidgain = 0;
-        //Check Bingos (3 rows, 3 columns, 2 diagonals)
+        Dictionary<int, int> Gains = new Dictionary<int, int>();
+        foreach (SymbolModel symbol in result)
+        {
+            if (!Gains.ContainsKey(symbol.ID))
+            {
+                if (symbol.ID >= 3 && symbol.ID <= 5)
+                {
+                    if (!Gains.ContainsKey(symbol.ID - 3))
+                    {
+                        Gains.Add(symbol.ID - 3, 1);
+                    }
+                    else
+                    {
+                        Gains[symbol.ID - 3] += 1;
+                    }
+                }
+                else
+                {
+                    Gains.Add(symbol.ID, 1);
+                }
+            }
+            else
+            {
+                Gains[symbol.ID]++;
+            }
+        }
+        int[] Bingos = new int[] { 1, 1, 1 };
+        int[][] BingoLines = new[]
+        {
+            new[] { 0, 1, 2 }, new[] { 3, 4, 5 }, new[] { 6, 7, 8 },
+            new[] { 0, 3, 6 }, new[] { 1, 4, 7 }, new[] { 2, 5, 8 },
+            new[] { 0, 4, 8 }, new[] { 2, 4, 6 },
+            new[] { 0, 1, 3, 4 }, new[] { 1, 2, 4, 5 }, new[] { 3, 4, 6, 7 }, new[] { 4, 5, 7, 8 }
+        };
+        foreach (int[] line in BingoLines)
+        {
+            int CheckResult = CheckBingo(result, line);
+            if (CheckResult >= 0)
+            {
+                Bingos[CheckResult]++;
+            }
+
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (Gains.ContainsKey(i))
+            {
+                Gains[i] *= Bingos[i];
+            }
+            
+        }
+        Debug.Log(string.Join(",", Gains));
+        //Debug.Log(string.Join(",", Bingos));
         //Send gain values
+    }
+        
+    
+
+    private int CheckBingo(List<SymbolModel> result, int[] line)
+    {
+        int curSymbol = -1;
+        foreach (int index in line)
+        {
+            if (curSymbol == -1)
+            {
+                curSymbol = result[index].ID;
+                if (curSymbol >= 6)
+                {
+                    return -1;
+                }
+                else if (curSymbol >= 3)
+                {
+                    curSymbol -= 3;
+                }
+            }
+            
+            else if (curSymbol != result[index].ID)
+            {
+                if (result[index].ID >= 3 && result[index].ID <= 5 && result[index].ID - 3 == curSymbol)
+                {
+                    continue;
+                }
+                return -1;
+            }
+        }
+        return curSymbol;
     }
 
 
